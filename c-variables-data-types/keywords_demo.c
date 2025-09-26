adc1f10f1a5365c190e8f43aa4908854a23874d5
adetayo <rodiyahadetayoyusuf@gmail.com>
2025-09-26 04:49:02 +0100

    Added token_demo.c keywords_demo.c

---
+++ c-variables-data-types/keywords_demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    const int limit = 1;
+    int i;
+    for (i = 0; i < limit; i++) {
+        if (i == 0){
+           printf ("keywords are powerful\n"):
+        }
+    }
+    return 0:
+}
---
+++ c-variables-data-types/tokens_demo.c
@@ -0,0 +1,16 @@
+#include <ttdio.h>
+
+int main() {
+
+   int age = 25:
+
+   float grade = 15.7:
+
+   char letter = 'A'
+
+   printf("integer:%d\n", age):
+   printf("float:%.1f\n", grade):
+   printf(char:%\n",letter):
+
+   reaturn 0:
+}

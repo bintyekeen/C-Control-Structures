5a8161fb4c8cd8b058c686c6c993542c56d7416d
adetayo <rodiyahadetayoyusuf@gmail.com>
2025-10-24 18:32:38 +0100

    C-Control-Structures

---
+++ my_document.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    printf("%d\n",!0);//output ??
+    printf("%d\n",!1);//output ??
+    printf("%d\n",!5);//output ??
+    printf("%d\n",!-19);//output??
+    return 0;
+}
